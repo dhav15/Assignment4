c12527b5ab304c3359d90809090d6d782d32f753
dhav15 <pate7400@mylaurier.ca>
2021-07-23 18:38:32 -0400

    Update a4.c

--- a4.c
+++ a4.c
@@ -1,9 +1,30 @@
-Authors: Dhavani Patel & Judy Praught
+/*
+ * Authors: Dhavani Patel (@dhav15) & Judy Praught (@Judy-Praught)
+ * URL:
+ */
 
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 
 int main(){
 
+	printf("Number of Customers: ");
+	printf("Currently Available resources: ");
+	printf("Maximum resources from file: ");
+	readFile();
 	return 0;
+}
+
+int readFile(){
+	FILE *file = fopen("sample4_in.txt", "r");
+	return 0;
+}
+
+int safetyAlgorithm(){
+
+	return 0;
+}
+
+void resourceRequest() {
+
 }
