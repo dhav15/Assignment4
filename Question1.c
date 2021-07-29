85708168b1b5ed88b2ce5329cb4db6a2dea8d6d0
Judy-Praught <judypraught@yahoo.com>
2021-07-29 14:35:01 -0400

    Add RQ and RL

---
+++ Question1.c
@@ -0,0 +1,244 @@
+/*
+ * Authors: Dhavani Patel (@dhav15) & Judy Praught (@Judy-Praught)
+ * URL:
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+#include <ctype.h>
+
+int available[5];
+int Max[5][4];
+int allocation[5][4];
+int need[5][4];
+
+
+
+
+
+void readFile();
+int resourceRequest(char *command);
+int resourceRelease(char *command);
+void printStatus();
+
+int main(int argc, char *argv[]){
+
+	printf("Number of Customers: 5\n");
+	printf("Currently Available resources:");
+	for (int i = 1; i < argc; i++) {
+		available[i - 1] = atoi(argv[i]);
+		printf(" %d", available[i - 1]);
+	}
+	printf("\nMaximum resources from file:\n");
+	readFile();
+	for (int i = 0; i < 5; i++){
+		for(int j = 0; j < 4; j++){
+			if(j != 3){
+				printf("%c,", Max[i][j]);
+			} else if(j == 3){
+				printf("%c", Max[i][j]);
+				printf("\n");
+			}
+		}
+	}
+	char command[20];
+	while(1) {
+		
+		printf("Enter Command: ");
+		fgets(command, 20, stdin); 
+		command[strcspn(command, "\n")] = 0;
+		int exit = strcmp(command, "EXIT");
+		int RQ = strncmp(command, "RQ", 2);
+		int RL = strncmp(command, "RL", 2);
+		if (exit == 0){
+			break;
+		} else if (RQ == 0){
+			resourceRequest(command);
+		} else if (RL == 0) {
+			resourceRelease(command);
+		} else if (strncmp(command, "Status", 6) == 0) {
+			printStatus();
+		} else {
+			printf("ERROR: Please enter valid command\n");
+		}
+		
+
+	}
+	return 0;
+}
+
+void readFile(){
+	//Get input array
+	int one_dArray[39]= {0};
+	int count = 0;
+	//Read file
+	FILE *fp;
+	fp = fopen("sample4_in.txt","r");
+	struct stat st;
+	fstat(fileno(fp), &st);
+	char *fileContent = (char*) malloc(((int) st.st_size + 1) * sizeof(char));
+	fileContent[0] = '\0';
+	while (!feof(fp)) {
+		count++;
+		char line[100];
+		if (fgets(line, 100, fp) != NULL) {
+			strncat(fileContent, line, strlen(line));
+		}
+
+
+	}
+	int j = 0;
+	for(int i = 0; i < 39; i = i +2){
+		one_dArray[j++] = fileContent[i];
+	}
+
+	memcpy(Max, one_dArray, 20 * sizeof(int));
+}
+
+/*int safetyAlgorithm(){
+	int work[m], finish[n], safeSeq[n];
+	int i, j, x = 0, counter = 5;
+	for (i = 0; i < m; i++) {
+		work[i] = available[i];
+	}
+	for (j = 0; j < n; j++) {
+		finish[j] = 0;
+	}
+	int isSafe = 0;
+	while (counter > 0) {
+		for (i = 0; i < n; i++) {
+			if (finish[i] == 0) {
+				for (j = 0; j < m; j++) {
+					if (need[i][j] <= work[j]) {
+						safeSeq[x] = i;
+						work[i][j] += allocation[j];
+						finish[i] = 1;
+						x++;
+					}
+				}
+
+			}
+		}
+	}
+	return 0;
+}*/
+
+int resourceRequest(char *command) {
+	char resources[4];
+	
+	int num;
+	char *token = strtok(command, " ");
+	int sucsess = 0;
+	
+	num = (command[3]- '0'); 
+	int j = 0;
+	for( int i = 0; i < 18; i++){
+		if( i > 3 && isdigit(command[i])){
+			resources[j] = (command[i] - '0');
+			j++; 
+			
+		} 
+	}
+	for (j = 0; j < 4; j++) {
+	
+		if (resources[j] > available[j]) {
+			printf("ERROR: Resources are not available\n");
+			//thread wait
+			//isAvail = 1;
+			sucsess = -1;
+			break;
+		}
+	
+	
+		else if ((resources[j]) > (Max[num][j]- '0')) {
+			printf("ERROR: Threads cannot request more than maximum number of resource\n");
+			sucsess = -1;
+			break;
+
+		
+		}
+		
+	}
+	
+	if (sucsess == 0) {
+		for (j = 0; j < 4; j++) {
+			available[j] = available[j] - resources[j];
+			allocation[num][j] = allocation[num][j] + resources[j];
+			need[num][j] = (Max[num][j]- '0') - allocation[num][j];
+		}
+		printf("State is safe, and request is satisfied\n");
+	}
+
+	return sucsess; //if successful, else return -1
+}
+
+int resourceRelease(char *command) {
+	
+	char resources[4];
+	char match;
+	int num;
+	char *token = strtok(command, " ");
+	int sucsess = 0;
+	
+	num = (command[3]- '0'); 
+	int j = 0;
+	for( int i = 0; i < 18; i++){
+		if( i > 3 && isdigit(command[i])){
+			resources[j] = (command[i] - '0');
+			j++; 
+		} 
+	}
+
+
+	for(int i = 0; i < 4; i++){
+		if (resources[i] <= allocation[num][i]){
+			available[i] = available[i] + resources[i];
+			allocation[num][i] = allocation[num][i] - resources[i];
+			need[num][i] = (Max[num][i]- '0') + allocation[num][i];	
+			}
+		else{
+			printf("ERROR: Cannot release unaquired resource\n");
+			sucsess = -1;
+			break;
+			}
+			
+				
+		}
+
+	return sucsess; //if successful, else return -1
+}
+
+void printStatus() {
+	int i, j;
+	printf("Available Resources:\n");
+	for (i = 0; i < 4; i++) {
+		printf("%d ", available[i]);
+	}
+	printf("\nMaximum Resources:\n");
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%d ", (Max[i][j]-'0'));
+		}
+		printf("\n");
+	}
+	printf("Allocated Resources:\n");
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%d ", allocation[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Need Resources:\n");
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%d ", need[i][j]);
+		}
+		printf("\n");
+	}
+}
